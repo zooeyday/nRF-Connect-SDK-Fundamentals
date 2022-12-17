2fe7c3c3cab40302539b1ffe677583df576a4a4b
zooeyday <zooeyday@hanmail.net>
2022-12-18 04:56:58 +0900

    Update main.c

--- v2.x.x/lesson7/fund_less7_exer1_solution/src/main.c
+++ v2.x.x/lesson7/fund_less7_exer1_solution/src/main.c
@@ -4,14 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
-#include <sys/printk.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
 /* STEP 2 - Define stack size and scheduling priority used by each thread */
 #define STACKSIZE 1024
 
 #define THREAD0_PRIORITY 7
-#define THREAD1_PRIORITY 7
+#define THREAD1_PRIORITY 8
 
 void thread0(void)
 {
@@ -32,9 +32,9 @@ void thread1(void)
 		  /* STEP 3 - Call printk() to display a simple string "Hello, I am thread1" */
           printk("Hello, I am thread1\n");		  
 		  /* STEP 8 - Make the thread yield */
-		  //k_yield();		  
+		  k_yield();		  
 		  /* STEP 10 - Put the thread to sleep */
-		  k_msleep(5);
+		  //k_msleep(5);
 		  /* Remember to comment out the line from STEP 8 */
 	}
 }
